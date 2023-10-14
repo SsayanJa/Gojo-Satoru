69be3baf4ff1c221c7e867578831ffec329bdc77
SsayanJa <145949770+SsayanJa@users.noreply.github.com>
2023-10-14 11:39:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = [' SAYAN ']
+global.premium = ['919339361276']
+global.ownernomer = '919339361276'
+global.ownername = 'SAYAN'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
